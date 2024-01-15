e1b16a10793a0abb5174122e8fb40a59053fd89a
Rafael Mecenas <rafaeljmecenas1@gmail.com>
2024-01-15 17:12:54 -0300

    Inicio novo Projeto de Quiz

---
+++ src/data/perguntas.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+export
+ const Perguntas  = [
+    {
+        pergunta:'Qual século estamos?',
+        opcoesResposta:[
+            {resposta:'20', correta:false, alternativa:'A)'},
+            {resposta:'22', correta:false, alternativa:'B)'},
+            {resposta:'21', correta:true, alternativa:'C)'},
+            {resposta:'19', correta:false, alternativa:'D)'},
+        ]
+    },
+    
+    {
+        pergunta:'Onde está localizado o rio Tiête?',
+        opcoesResposta:[
+            {resposta:'Minas Gerais', correta:false, alternativa:'A)'},
+            {resposta:'São Paulo', correta:true, alternativa:'B)'},
+            {resposta:'Santos', correta:false, alternativa:'C)'},
+            {resposta:'Maranhão', correta:false, alternativa:'D)'},
+        ]
+    },
+    {
+        pergunta: 'Qual o nome de Pitico',
+        opcoesResposta:[
+            {resposta:'Pitico', correta:false, alternativa:'A)'},
+            {resposta:'João Pedro', correta:true, alternativa:'B)'},
+            {resposta:'Caneta Azul', correta:false, alternativa:'C)'},
+            {resposta:'João Carlos', correta:false, alternativa:'D)'},
+        ]
+    }
+]
